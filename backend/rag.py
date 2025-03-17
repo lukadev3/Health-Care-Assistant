096a2709489a10a23a769a18e4fda763e54ea122
lukadev3 <lukaivanovic835@gmail.com>
2025-03-17 20:56:48 +0100

    Initial pipeline done

--- backend/rag.py
+++ backend/rag.py
@@ -1,15 +1,16 @@
 import os
 import chromadb
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, SummaryIndex
-from llama_index.core.objects import ObjectIndex
+import fitz
+import tempfile
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, SummaryIndex, load_index_from_storage
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.tools import QueryEngineTool
-from llama_index.core.agent import FunctionCallingAgentWorker
-from llama_index.core.agent import AgentRunner
+from llama_index.core.agent import ReActAgent
 from llama_index.vector_stores.chroma import ChromaVectorStore
-from llama_index.llms.mistralai import MistralAI
 from llama_index.core import Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+#from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,69 +18,103 @@ load_dotenv()
 CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH")
 EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME")
 LLM_MODEL_NAME = os.getenv("LLM_MODEL_NAME")
-MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+API_TOKEN = os.getenv("API_TOKEN")
 
 Settings.embed_model = HuggingFaceEmbedding(model_name=EMBEDDING_MODEL_NAME)
-Settings.llm = MistralAI(model=LLM_MODEL_NAME, api_key=MISTRAL_API_KEY)
+Settings.llm = HuggingFaceInferenceAPI(model_name=LLM_MODEL_NAME, token=API_TOKEN)
 
 chroma_client = chromadb.PersistentClient(path=CHROMA_DB_PATH)
 chroma_collection = chroma_client.get_or_create_collection("pdf_vectors")
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
+def extract_text_from_pdf(file_path: str) -> str:
+
+    """Extract text from the PDF file using PyMuPDF."""
+    pdf_document = fitz.open(file_path)
+    full_text = ""
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        full_text += page.get_text("text")  
+    print(full_text)
+    return full_text
+
 
 def handle_upload(file_path: str, name: str) -> str:
-    """Get vector query and summary query tools from a document."""
-
+    
+    """Get vector query and summary query tools from a document.
+       If indexes for related file exist, function load them and make vector and summary tools.
+       Otherwise, create indexes and tools from the file."""
+    
     name = os.path.splitext(name)[0]
     
-    documents = SimpleDirectoryReader(input_files=[file_path]).load_data()
-    splitter = SentenceSplitter(chunk_size=1024)
-    nodes = splitter.get_nodes_from_documents(documents)
+    try: 
+        vector_index = load_index_from_storage(storage_context, f"vector_index_{name}") 
+        summary_index = load_index_from_storage(storage_context, f"summary_index_{name}")
+        vector_query_tool = QueryEngineTool.from_defaults(
+            name=f"vector_tool_{name}",
+            query_engine=vector_index.as_query_engine(
+                similarity_top_k=3
+            ),
+            description=f"Useful for giving specific answer on question related to {name}"
+        )
+        summary_tool = QueryEngineTool.from_defaults(
+            name=f"summary_tool_{name}",
+            query_engine=summary_index.as_query_engine(
+                response_mode="tree_summarize",
+            ),
+            description=f"Useful for summarization questions related to {name}"
+        )
+    except:
+        full_text = extract_text_from_pdf(file_path)
+        
+        with tempfile.NamedTemporaryFile(delete=False, mode="w", encoding="utf-8") as temp_file:
+            temp_file.write(full_text)
+            temp_file_path = temp_file.name
 
-    vector_index = VectorStoreIndex(nodes, storage_context=storage_context)
-    query_engine = vector_index.as_query_engine(
-        similarity_top_k=3
-    )
-    vector_query_tool = QueryEngineTool.from_defaults(
-        name=f"vector_tool_{name}",
-        query_engine=query_engine,
-        description=f"Useful for giving specific answer on question related to {name}"
-    )
+        documents = SimpleDirectoryReader(input_files=[temp_file_path]).load_data()
+        splitter = SentenceSplitter(chunk_size=1024)
+        nodes = splitter.get_nodes_from_documents(documents)
 
-    summary_index = SummaryIndex(nodes)
-    summary_query_engine = summary_index.as_query_engine(
-        response_mode="tree_summarize",
-        use_async=True,
-    )
-    summary_tool = QueryEngineTool.from_defaults(
-        name=f"summary_tool_{name}",
-        query_engine=summary_query_engine,
-        description=f"Useful for summarization questions related to {name}"
-    )
+        vector_index = VectorStoreIndex(nodes, storage_context=storage_context)
+        vector_index.set_index_id(f"vector_index_{name}")
+        
+        query_engine = vector_index.as_query_engine(
+            similarity_top_k=3
+        )
+        vector_query_tool = QueryEngineTool.from_defaults(
+            name=f"vector_tool_{name}",
+            query_engine=query_engine,
+            description=f"Useful for giving specific answer on question related to {name}"
+        )
+
+        summary_index = SummaryIndex(nodes, storage_context=storage_context)
+        summary_index.set_index_id(f"summary_index_{name}")
+        summary_query_engine = summary_index.as_query_engine(
+            response_mode="tree_summarize"
+        )
+        summary_tool = QueryEngineTool.from_defaults(
+            name=f"summary_tool_{name}",
+            query_engine=summary_query_engine,
+            description=f"Useful for summarization questions related to {name}"
+        )
 
     return vector_query_tool, summary_tool
 
 
 def query_tools(query: str, tools: dict) -> str:
+
+    """Create an agent from the tools and query the agent with the query."""
     
-    obj_index = ObjectIndex.from_objects(
-        tools,
-        index_cls=VectorStoreIndex,
-    )
-
-    obj_retriever = obj_index.as_retriever(similarity_top_k=3)
-
-    agent_worker = FunctionCallingAgentWorker.from_tools(
-        tool_retriever=obj_retriever,  
-        llm = Settings.llm,
+    agent = ReActAgent.from_tools(
+        tools=tools,  
+        llm=Settings.llm,
         system_prompt=""" \
-            You are an agent designed to answer queries over a set of given papers.
-            Please always use the tools provided to answer a question. Do not rely on prior knowledge.\
+            You are an agent designed to answer queries using the available tools.
+            Always respond with the tools provided and do not rely on prior knowledge. 
+            Use the most relevant tool to answer the user's question.\
             """,
         verbose=True
     )
-    agent = AgentRunner(agent_worker)
-    
-    response = agent.chat(query)
+    response = agent.query(query)
     return str(response)
